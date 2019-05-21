3a05c19b304411a72f9aafac3a507e4447d6db7d
tianyuhang <tatamakui@tianyuhangdeMacBook-Pro.local>
2019-05-21 19:51:18 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MoorUI from './index.js'
+Vue.use(MoorUI)
 
 Vue.config.productionTip = false
 
